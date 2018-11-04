7e8e59f59e2639e63b0dc328dc330f601cb86a0a
momonala <mnalavadi@gmail.com>
2018-11-04 23:19:16 +0100

    clean up loops

--- mp3_to_piano.py
+++ mp3_to_piano.py
@@ -11,13 +11,14 @@ sample_rate = 100
 for offset in tqdm(range(0, s.ms, sample_rate)):
     sample_data = s.sample(offset=offset)
     piano_lit = s.generate_keyboard(sample_data)
-    frames.append(piano_lit)
-    
-clips = [ImageClip(f).set_duration(sample_rate/1000.) for f in frames]
-out = concatenate_videoclips(clips, method="compose")
+    single_frame = ImageClip(f).set_duration(sample_rate / 1000.)
+    frames.append(single_frame)
+
+out = concatenate_videoclips(frames, method="compose")
 out = out.set_audio(AudioFileClip(song_file))
+# temp audio to dealth with moviepy bug
 out.write_videofile("test.mp4",
-                    fps=1000//sample_rate,
+                    fps=1000 // sample_rate,
                     temp_audiofile="temp-audio.m4a",
                     remove_temp=True,
                     codec="libx264",
