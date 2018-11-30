452ed5f2df38074d0304977031fae00d345b019b
Mohit Nalavadi <mnalavadi@gmail.com>
2018-11-30 15:37:30 +0100

    youtube-dl bug workaround and better error msgs

--- mp3_to_piano.py
+++ mp3_to_piano.py
@@ -5,8 +5,10 @@ import logging
 import os
 import sys
 
-from decomposer import Decomposer
 import youtube_dl
+from pydub import AudioSegment
+
+from decomposer import Decomposer
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,24 +20,25 @@ parser.add_argument('-p', '--plot', default=False, type=bool)
 
 args = parser.parse_args()
 
+# download the song from youtube as video, cvt to mp3, cleanup
 if args.youtube:
     raw_name = args.youtube.split('=')[-1]
     options = {
+        # todo youtube-dl issue, cant download only audio, getting codec issue
+        'outtmpl': '%(id)s',
         'format': 'bestaudio/best',
-        'extractaudio': True,  # only keep the audio
-        'audioformat': "mp3",  # convert to mp3
-        'outtmpl': '%(id)s',   # name the file the ID of the video
-        'noplaylist': True,    # only download single song, not playlist
     }
-
     with youtube_dl.YoutubeDL(options) as ydl:
         a = ydl.download([args.youtube])
-        mp3_name = raw_name + '.mp3'
-        os.rename(raw_name, mp3_name)
-        logger.info(f'Sucessfully downloaded video file {mp3_name}.')
+        logger.info(f'Sucessfully downloaded video file {raw_name}.')
 
+    mp3_name = raw_name + '.mp3'
+    AudioSegment.from_file(raw_name).export(mp3_name, format="mp3")
+    logger.info(f'Sucessfully converted video file to mp3: {mp3_name}.')
     song_file = os.path.join('assets', mp3_name)
+
     try:
+        os.remove(raw_name)  # clean up
         os.rename(mp3_name, song_file)
     except FileExistsError:
         pass
