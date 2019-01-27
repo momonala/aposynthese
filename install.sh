da5a38e46306585eb21ca004aa19fbc506889af7
Mohit Nalavadi <mnalavadi@gmail.com>
2019-01-27 20:25:51 +0100

    update npm install command

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
-#! bin/bash
-
+#!/usr/bin/env bash
 
 # Install dependencies, source code
 sudo apt-get update
 sudo apt-get install -y ffmpeg nodejs npm python3-pip git
 git clone https://github.com/momonala/aposynthese.git
 cd aposynthese
-npm install
+sudo npm install
 pip3 install -r requirements.txt
 
 # Run the app
